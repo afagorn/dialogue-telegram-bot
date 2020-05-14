44c993066edb325d4917fcc3cdc943a788a0f1ab
afagorn <afagorn@gmail.com>
2020-05-14 17:56:25 +0300

    Fix bot_dev

---
+++ dev/bot_dev.php
@@ -0,0 +1,30 @@
+<?php
+
+use afagorn\DialogueTelegramBot\examples\commands\StartCommand;
+use afagorn\DialogueTelegramBotSDK\DialogueBot;
+use afagorn\DialogueTelegramBotSDK\TelegramAPI;
+
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor'  . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+/*-----------*/
+
+$dialogueBot = new DialogueBot(new TelegramAPI(
+    getenv("TELEGRAM_BOT_TOKEN"),
+    new \afagorn\DialogueTelegramBotSDK\DTO\ProxyDTO(
+        getenv("PROXY_SOCKS_IP"),
+        getenv("PROXY_SOCKS_PORT"),
+        getenv("PROXY_SOCKS_LOGIN"),
+        getenv("PROXY_SOCKS_PASSWORD")
+    )
+));
+
+$dialogueBot->getUpdatesHandler()->configGetUpdatesMethod(10);
+
+$dialogueBot->addCommands([
+    new StartCommand()
+]);
+
+$dialogueBot->startUpdatesLoop();
